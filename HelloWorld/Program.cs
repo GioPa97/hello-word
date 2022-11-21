826568b071d164dad424c3f60ecab39b201023f2
GioPa97 <giovannipagliara97@.com>
2022-11-21 11:51:49 +0100

    Aggiungere i file di progetto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var risposta = Console.ReadLine();
+
+Console.WriteLine("La rispostà inserita dall utente è: " + risposta);
+
+      
