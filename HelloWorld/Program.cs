f57a65e7bf8bcd8ef6972c1e44e9b89ca20f984c
GioPa97 <giovannipagliara97@.com>
2022-11-21 12:03:33 +0100

    Cambiato input iniziale

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Che giorno è oggi?");
 var risposta = Console.ReadLine();
-
 Console.WriteLine("La rispostà inserita dall utente è: " + risposta);
-
-      
